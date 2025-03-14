0d76bf207672acb0ef7ce7bbb204aa96389d508e
raulpesilva <adraulpesilva@gmail.com>
2025-03-14 00:33:22 -0300

    chore: log

--- src/actions/transactions.ts
+++ src/actions/transactions.ts
@@ -145,5 +145,6 @@ export const updateTransactionAction = async (_state: UpdateTransactionResponse,
 
 export const deleteTransactionAction = async (id: string, formData: FormData) => {
   await deleteTransaction(id);
+  console.log(formData);
   revalidatePath('/dashboard');
 };
